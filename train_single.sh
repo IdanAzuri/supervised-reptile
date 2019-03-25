2b5cdc375a008eceb66115d362a62d0e69677bcf
idanazuri <idan.azuri@gmail.com>
2019-03-25 15:14:01 +0200

    minor

--- train_single.sh
+++ train_single.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --mem=8g
-#SBATCH -c 8
-#SBATCH --gres=gpu:m60:1
+#SBATCH --mem=2g
+#SBATCH -c 2
+#SBATCH --gres=gpu:m60:2
 #SBATCH --time=1-20
 #SBATCH --mail-user=idan.azuri@mail.huji.ac.il
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
