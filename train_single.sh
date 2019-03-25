89a2f9f84df27d682d9cf6d0bbe95f48176e5eed
idanazuri <idan.azuri@gmail.com>
2019-03-25 15:10:01 +0200

    minor

--- train_single.sh
+++ train_single.sh
@@ -13,7 +13,7 @@ module load nvidia
 dir=/cs/labs/daphna/idan.azuri/unsupervised-reptile
 
 cd $dir
-source /cs/labs/daphna/idan.azuri/venv_64/bin/activate
+source /cs/labs/daphna/idan.azuri/venv3.6/bin/activate
 
 
 python3 run_miniimagenet.py --shots 1 --inner-batch 10 --inner-iters 8 --meta-step 1 --meta-batch 5 --meta-iters 100000 --eval-batch 5 --eval-iters 50 --learning-rate 0.001 --meta-step-final 0 --train-shots 10 --checkpoint ckpt_m153
