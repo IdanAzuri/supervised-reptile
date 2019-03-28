4286788a6c88cbf47cf250191eb31563219a1229
idanazuri <idan.azuri@gmail.com>
2019-03-28 14:39:38 +0200

    minor

--- run_miniimagenet.py
+++ run_miniimagenet.py
@@ -35,8 +35,8 @@ def main():
         print('Evaluating...')
         eval_kwargs = evaluate_kwargs(args)
         print('Train accuracy: ' + str(evaluate(sess, model, train_set, **eval_kwargs)))
-        print('Validation accuracy: ' + str(evaluate(sess, model, val_set, **eval_kwargs)))
         print('Test accuracy: ' + str(evaluate(sess, model, test_set, **eval_kwargs)))
+        print('Validation accuracy: ' + str(evaluate(sess, model, val_set, **eval_kwargs)))
 
 if __name__ == '__main__':
     main()
